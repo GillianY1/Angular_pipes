dffed68b57872b30e204800436f260945bf58ae7
GillianY1 <gilliangilliany@gmail.com>
2020-09-27 22:07:17 +0800

    add currency pipes , decimalPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name: string;
   date: string;
+  amount: number;
+  height: number;
   onNameChange(value: string){
     this.name = value;
   }
   onDateChange(value: string){
     this.date = value;
+  }
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
   }
 }
